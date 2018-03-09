ab2ff5bfc5e9c81bc17152122d0477b832e58136
Karin Christensen <karin.christensen@chasacademy.se>
2018-03-09 14:35:31 +0100

    Add eventlistensers, search and renderPhotos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// import './styles/app.scss';
 import './styles/style.scss';
 import {on, sanitizeString, getPromiseData, axelVisar, flatten} from './helpers/';
 
@@ -6,43 +5,80 @@ class Mashed {
   constructor(element) {
     this.root = element;
 
-    this.fetchPhotos();
-    this.fetchWords('hippopotamus');
+    this.addEventListeners();
   }
 
-  fetchPhotos() {
+  addEventListeners() {
+    const input = document.querySelector('.form-control');
+    const searchBtn = document.querySelector('button');
+
+    searchBtn.addEventListener('click', () => {
+      this.fetchWords(input.value);
+      this.fetchPhotos(input.value);
+    });
+  }
+
+  search() {
+    let searchQuery = this.input.value;
+
+    let apiCalls = [
+      this.fetchPhotos(searchQuery),
+      this.fetchWords(searchQuery)
+    ];  
+
+    Promise.all(apiCalls)
+      .then((result) => {
+        return result.map(type => type.json());
+      })
+      .catch(reject);
+  }
+
+  renderPhotos(photos) {
+    const photoRes = document.querySelector('.photo-list');
+    photoRes.innerHTML = "";
+
+    photos.forEach((photo) => {
+      const photoList = document.createElement('li');
+
+      photoList.style.backgroundImage = `url(${photo.url_o || photo.url_q})`;
+      photoList.classList.add('result');
+
+      photoRes.appendChild(photoList);
+    });
+  }
+
+  fetchPhotos(searchQuery) {
     let resourceUrl =
       'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
     let flickrAPIkey = process.env.FLICKR_API_KEY;
 
     let flickrQueryParams =
-      '&text=space' +
-      '&extras=url_q&format=json&nojsoncallback=1' +
+      '&text=' + searchQuery + 
+      '&extras=original_format,url_q,url_o&format=json&nojsoncallback=1' +
       '&per_page=5'
     let flickrUrl = resourceUrl + flickrAPIkey + flickrQueryParams
 
-    fetch(flickrUrl)
+    return fetch(flickrUrl)
       .then(res => res.json())
       .then(res => {
-        console.log('Test FlickR!')
-        console.log(res)
+        this.renderPhotos(res.photos.photo);
       })
       .catch(err => console.error(err))
   }
 
-  fetchWords(query) {
-    let wordLabAPIkey = process.env.BHT_API_KEY;
-    let wordLabUrl = `http://words.bighugelabs.com/api/2/${wordLabAPIkey}/${query}/json`
+  fetchWords(searchQuery) {
+    let wordLabAPIkey = process.env.BHT_API_KEY
+    let wordLabUrl = `http://words.bighugelabs.com/api/2/${wordLabAPIkey}/${searchQuery}/json`
 
-    fetch(wordLabUrl)
+    return fetch(wordLabUrl)
       .then(res => res.json())
       .then(res => {
         console.log('Test BigHugeLabs!')
         console.log(res)
-      })
-      .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
   }
-}
+} 
 
 (function() {
   new Mashed(document.querySelector('#mashed'))
