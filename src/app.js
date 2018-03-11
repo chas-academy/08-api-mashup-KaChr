4e7db2d200dfb240f7f708f3a42931eab55f8a7f
Karin Christensen <karin.christensen@chasacademy.se>
2018-03-11 22:50:12 +0100

    Add renderWords

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ class Mashed {
   constructor(element) {
     this.root = element;
 
+    this.search = this.search.bind(this);
     this.addEventListeners();
   }
 
@@ -12,12 +13,9 @@ class Mashed {
     const input = document.querySelector('.form-control');
     const searchBtn = document.querySelector('button');
 
-    searchBtn.addEventListener('click', () => {
-      this.fetchWords(input.value);
-      this.fetchPhotos(input.value);
-    });
+    searchBtn.addEventListener('click', this.search);
   }
-
+ 
   search() {
     let searchQuery = this.input.value;
 
@@ -27,23 +25,48 @@ class Mashed {
     ];  
 
     Promise.all(apiCalls)
-      .then((result) => {
-        return result.map(type => type.json());
+      .then(result => {
+        this.renderPhotos(res[0])
+        this.renderWords(res[1])
       })
       .catch(reject);
   }
 
-  renderPhotos(photos) {
+  renderPhotos(data) {
+    const photos = data.photos.photo;
+
     const photoRes = document.querySelector('.photo-list');
     photoRes.innerHTML = "";
 
-    photos.forEach((photo) => {
+    photos.map((photo) => {
       const photoList = document.createElement('li');
 
       photoList.style.backgroundImage = `url(${photo.url_o || photo.url_q})`;
       photoList.classList.add('result');
 
       photoRes.appendChild(photoList);
+    });
+  }
+
+  renderWords(data) {
+    let words = Object.keys(data).map(key => {
+      return Object.values(data[key]).map(word => {
+        return word;
+      });
+    });
+
+    const wordRes = document.querySelector('.words');
+    wordRes.innerHTML = "";
+
+    words.map((word) => {
+      const wordList = document.createElement('li');
+      const wordAnchor = document.createElement('a');
+
+      wordAnchor.href = '#';
+      wordAnchor.textContent = word;
+
+      wordRes.appendChild(wordList);
+      wordList.appendChild(wordAnchor);
     });
   }
 
@@ -58,25 +81,25 @@ class Mashed {
       '&per_page=5'
     let flickrUrl = resourceUrl + flickrAPIkey + flickrQueryParams
 
-    return fetch(flickrUrl)
-      .then(res => res.json())
-      .then(res => {
-        this.renderPhotos(res.photos.photo);
-      })
-      .catch(err => console.error(err))
+    return fetch(flickrUrl);
+      // .then(res => res.json())
+      // .then(res => {
+      //   this.renderPhotos(res.photos.photo);
+      // })
+      // .catch(err => console.error(err))
   }
 
   fetchWords(searchQuery) {
     let wordLabAPIkey = process.env.BHT_API_KEY
     let wordLabUrl = `http://words.bighugelabs.com/api/2/${wordLabAPIkey}/${searchQuery}/json`
 
-    return fetch(wordLabUrl)
-      .then(res => res.json())
-      .then(res => {
-        console.log('Test BigHugeLabs!')
-        console.log(res)
-    })
-    .catch(err => console.error(err))
+    return fetch(wordLabUrl);
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     console.log('Test BigHugeLabs!')
+    //     console.log(res)
+    // })
+    // .catch(err => console.error(err))
   }
 } 
 
