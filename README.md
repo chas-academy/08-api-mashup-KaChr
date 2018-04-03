449e935001699afeaa21d30136c595eef8bb7f2d
Karin Christensen <karin.christensen@chasacademy.se>
2018-04-03 11:58:58 +0200

    Update deploy to work on all platforms

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 08-api-mashup-KaChr
 http://karinchristensen.chas.academy/08-api-mashup
+
+### Deploy instructions
+1. Build the project in production mode with webpack: ```webpack --env=prod```
+2. Run the deploy script: ```./deploy live go``` (this requires rsync to be installed, is available in ubuntu for windows)
+3. Will prompt for user password, update `deploy.sh` to match your ssh username and path on your hosting provider.
+3. Take a break
+4. Visit the production URL
+
+### Re-deploy instructions
+See above
--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
-$ERRORSTRING="Något gick åt skogen. Kolla så parametrarna som skickas in är rätt"
+ERRORSTRING="Error. Please make sure you've indicated correct parameters"
 if [ $# -eq 0 ]
     then
-        echo $ERRORSTRING;
-elif [ $1 == "live" ]
+        echo "$ERRORSTRING";
+elif [[ "$1"=="live" ]]
     then
         if [[ -z $2 ]]
             then
                 echo "Testkörning (inget händer)"
-                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./ 226753_kachr@ssh.binero.se:/storage/content/53/226753/karinchristensen.chas.academy/public_html/08-api-mashup-KaChr
-        elif [ $2 == "go" ]
+                rsync --dry-run -azv --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ 226753_kachr@ssh.binero.se:/storage/content/53/226753/karinchristensen.chas.academy/public_html/08-api-mashup-KaChr
+        elif [[ "$2"=="go" ]]
             then
                 echo "Kör den riktiga deployen"
-                rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./ 226753_kachr@ssh.binero.se:/storage/content/53/226753/karinchristensen.chas.academy/public_html/08-api-mashup-KaChr
+                rsync -azv --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ 226753_kachr@ssh.binero.se:/storage/content/53/226753/karinchristensen.chas.academy/public_html/08-api-mashup-KaChr
         else
-            echo $ERRORSTRING;
+            echo "$ERRORSTRING";
         fi
 fi
